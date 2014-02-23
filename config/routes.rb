1a4ad87e30d6732e027438fabd6e1e65aca010fb
J P <jamie.pryde.09@aberdeen.ac.uk>
2014-02-23 22:30:45 +0000

    routes altered, beta1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Storygen::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'button#home'
+  get 'button/home'
+  get 'button/story'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
