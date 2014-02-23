eb6447f15dc56c7ac1f8aa2ff8d8ad3feb24593b
J P <jamie.pryde.09@aberdeen.ac.uk>
2014-02-23 22:25:50 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storygen::Application.config.secret_key_base = '93f25167b4d6f81adc6a7a8bce77af6884b9b713dc806321e14fb04930e69b6b90b6b0c5ebe87a2a0805f64b48c74fef8ba15264d0b75e43076bd9b4e5ae30f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Storygen::Application.config.session_store :cookie_store, key: '_storygen_session'
